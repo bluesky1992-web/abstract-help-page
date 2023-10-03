1fd15180cd9512a51bc61d352d8f7a9f2e81b11f
Ali ibrahim <ali.ibrahim.jasim@gmail.com>
2023-10-03 19:44:13 +0300

    items centered

--- src/components/Services.js
+++ src/components/Services.js
@@ -3,14 +3,18 @@ import services from '../db';
 
 export default function Services() {
   return (
-    <div className='m-5 '>
+    <div className='py-20 grid sm:grid-cols-2 gap-9 justify-center items-center sm:ml-14  grid-cols-1 px-5 '>
       {services.map((service) => (
-        <div className='flex ' key={service.id} >
-          <img className='w-[5%] h-[5%] mx-4' src={service.img} alt={service.title} />
-          <div>
-            <h3 className='text-2xl font-bold'>{service.title}</h3>
-            <p className='w-1/2 '>{service.body}</p>
-            <a className='text-[#4c5fd5]' href='#'>Learn more...</a>
+        <div className='' key={service.id}>
+          <div className='flex  '>
+            <img className='w-16 h-16 mx-4' src={service.img} alt={service.title} />
+            <div>
+              <h3 className='text-2xl font-bold'>{service.title}</h3>
+              <p className=' max-w-xs text-2xl break-words'>{service.body}</p>
+              <a className='text-[#4C5FD5] text-xl' href='#'>
+                Learn more...
+              </a>
+            </div>
           </div>
         </div>
       ))}
